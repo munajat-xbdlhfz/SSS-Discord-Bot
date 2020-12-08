2553a667105a8e2f8cc43830f20752b7a76fd5fb
Munajat A.H <munajatabdulhafiz18@gmail.com>
2020-12-09 01:20:05 +0700

    Fix welcome embed

--- commands/welcome.js
+++ commands/welcome.js
@@ -6,14 +6,15 @@ module.exports = {
     execute(guildMember, Discord) {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#0000FF')
-        .setTitle('WELCOME TO OUR DISCORD SERVER!')
-        .setDescription(
-            `
-            Selamat datang <@${guildMember.user.id}>
-            Selamat bergabung di SSS Discord Server
-            Silahkan membaca <#${process.env.RULES_ID}> terlebih dahulu
-            Dan jangan lupa untuk mengisi <#${process.env.GREETINGS_ID}> biar akrab
-            `)
+        .addFields(
+            {
+                name: 'WELCOME TO OUR DISCORD SERVER!', 
+                value: `Selamat datang <@${guildMember.user.id}> \n`+
+                        `Selamat bergabung di SSS Discord Server \n`+
+                        `Silahkan membaca <#${process.env.RULES_ID}> terlebih dahulu \n`+
+                        `Dan jangan lupa untuk mengisi <#${process.env.GREETINGS_ID}> biar akrab`
+            },
+        )
         .setThumbnail(guildMember.user.displayAvatarURL())
         .setImage("https://cdn.discordapp.com/attachments/732668771231334501/785608609760936017/people.jpg")
         // .setTimestamp()
