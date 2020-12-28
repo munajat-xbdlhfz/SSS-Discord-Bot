c0de34c42d136fdd4f6ecc5fc41e471dc49b799c
munajat-xbdlhfz <munajatabdulhafiz18@gmail.com>
2020-12-28 12:52:28 +0700

    Fix claim daily and weekly

--- functions/claim.js
+++ functions/claim.js
@@ -27,7 +27,7 @@ const claimDaily = async (guildId, userId) => {
                 }
             }
             
-            await dailyPrimogemsSchema.findOneAndUpdate(userData, userData, {
+            await dailyPrimogemsSchema.findOneAndUpdate(userData, claim, {
                 upsert: true
             })
 
@@ -72,7 +72,7 @@ const claimWeekly = async (guildId, userId) => {
                 }
             }
             
-            await weeklyPrimogemsSchema.findOneAndUpdate(userData, userData, {
+            await weeklyPrimogemsSchema.findOneAndUpdate(userData, claim, {
                 upsert: true
             })
 
