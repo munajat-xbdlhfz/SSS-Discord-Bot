3ac6a8cd712722c4ea153dd8600ee1aa6ac228f2
munajat-xbdlhfz <munajatabdulhafiz18@gmail.com>
2020-12-28 11:45:12 +0700

    Change mongo path

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoPath = process.env.MONGO_PATH
+const mongoPath = process.env.MONGO_URL
 
 module.exports= async () => {
     await mongoose.connect(mongoPath, {
