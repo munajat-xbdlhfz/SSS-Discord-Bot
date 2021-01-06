8d80eedc14c8a96a665e166a863a2396ee3f50bd
munajat-xbdlhfz <munajatabdulhafiz18@gmail.com>
2021-01-07 04:20:22 +0700

    Add character Hu Tao and Rosaria

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 // GENSHIN IMPACT CONSTANTS
 
 // CHARACTER RARITY
-const characters5Star = ['Diluc', 'Klee', 'Keqing', 'Jean', 'Venti', 'Xiao', 'Tartaglia', 'Mona', 'Qiqi', 'Ganyu', 'Kamisato Ayaka', 'Zhongli', 'Albedo'];
-const characters4Star = ['Fischl', 'Beidou', 'Lisa', 'Razor', 'Amber', 'Xiangling', 'Bennett', 'Xinyan', 'Xingqiu', 'Barbara', 'Diona', 'Chongyun', 'Kaeya', 'Ningguang', 'Noelle', 'Sucrose']
+const characters5Star = ['Diluc', 'Klee', 'Hu Tao', 'Keqing', 'Jean', 'Venti', 'Xiao', 'Tartaglia', 'Mona', 'Qiqi', 'Ganyu', 'Kamisato Ayaka', 'Zhongli', 'Albedo'];
+const characters4Star = ['Fischl', 'Beidou', 'Lisa', 'Razor', 'Amber', 'Xiangling', 'Bennett', 'Xinyan', 'Xingqiu', 'Barbara', 'Diona', 'Chongyun', 'Kaeya', 'Rosaria', 'Ningguang', 'Noelle', 'Sucrose']
 
 // ALL TYPE OFF 4 STAR WEAPONS
 const sword4Star = ['Favonius Sword', `Lion's Roar`, 'Sacrificial Sword', 'Sword of Descension', 'The Alley Flash', 'The Flute'];
@@ -14,7 +14,7 @@ const bows4Star = ['Favonius Warbow', 'Rust', 'Sacrificial Bow', 'The Stringless
 // WEAPON RARITY
 const weapons5Star = [
     'Aquila Favonia', 'Skyward Blade', 'Summit Shaper',
-    `Kunwu's Iris Rift`, 'Primordial Jade Winged Spear', 'Skyward Spine', 'Vortex Vanquisher',
+    'Primordial Jade Winged Spear', 'Skyward Spine', 'Vortex Vanquisher',
     'Skyward Pride', 'The Unforged', `Wolf's Gravestone`,
     'Lost Prayer to the Sacred Winds', 'Memory of Dust', 'Skyward Atlas',
     `Amos' Bow`, 'Skyward Harp',
@@ -23,7 +23,7 @@ const weapons4Star = [...bows4Star, ...catalyst4Star, ...claymore4Star, ...polea
 const weapons3Star = [
     'Skyrider Sword', 'Harbringer of Dawn', 'Cool Steel', 'Fillet Blade', 'Dark Iron Sword', 
     'Black Tessel', 'Halberd', 
-    'Bloodtainted Greatsword', 'Debate Club', 'Ferrous Shadow', 'Quartz', 'Skyrider Greatsword', 'White Iron Greatsword', 
+    'Bloodtainted Greatsword', 'Debate Club', 'Ferrous Shadow', 'Skyrider Greatsword', 'White Iron Greatsword', 
     'Amber Catalyst', 'Emerald Orb', 'Magic Guide', 'Otherworldly Story', 'Thrilling Tales of Dragon Slayers', 'Twin Nephrite', 
     'Ebony Bow', 'Messenger', 'Raven Bow', 'Recurve Bow', `Sharpshooter's Oath`, 'Slingshot'
 ]
