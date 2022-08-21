7a1c5f340f0f2dfe858f538cc9dbbe534674bfea
unknown <munajatabdulhafiz18@gmail.com>
2022-08-21 20:04:57 +0700

    fix canvas attachment on GuildMemberAdd

--- Events/Member/GuildMemberAdd.js
+++ Events/Member/GuildMemberAdd.js
@@ -1,14 +1,12 @@
 const { 
     Client, 
-    CommandInteraction, 
     ChatInputCommandInteraction,  
     EmbedBuilder,
     AttachmentBuilder, 
-    GuildMember,
     WebhookClient
 } = require("discord.js")
-const Canvas = require("canvas")
-require("dotenv")
+const Canvas =  require("@napi-rs/canvas");
+require("dotenv").config();
 
 module.exports = {
     name: "guildMemberAdd",
@@ -20,59 +18,50 @@ module.exports = {
     async execute(intearction, client) {
         const { user, guild } = intearction
 
-        // TEST SENDING CONSOLE.LOG 1
-        console.log("EXECUTE 1")
-
         const webhook = new WebhookClient({ url: process.env.WELCOME_WEBHOOK_URL });
 
-        Canvas.registerFont('./Fonts/HYWenHei.ttf', { family: 'HYWenHei', style: 'Heavy', weight: 'Bold' })
-        const canvas = Canvas.createCanvas(960, 540);
         let fontSize = 50;
-        const ctx = canvas.getContext("2d");
+        const canvas = Canvas.createCanvas(960, 540);
+        const context = canvas.getContext("2d");
+        const background = await Canvas.loadImage('./Structures/Images/welcome.png');
+        Canvas.GlobalFonts.registerFromPath('./Fonts/HYWenHei.ttf')
 
-        // TEST SENDING CONSOLE.LOG 2
-        console.log("EXECUTE 2")
+        context.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-        // Load Image Background
-        const background = await Canvas.loadImage("./Structures/Images/welcome.png");
-        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-        ctx.shadowColor = "BLACK";
-        ctx.shadowBlur = 10;
-        ctx.strokeStyle = "#9B59B6";
-        ctx.strokeRect(0, 0, canvas.width, canvas.height);
+        // Set Text Stroke
+        context.shadowColor = "BLACK";
+        context.shadowBlur = 10;
+        context.strokeStyle = "#9B59B6";
+        context.strokeRect(0, 0, canvas.width, canvas.height);
 
         // Print Welcome Text
-        ctx.font = "70px HYWenHei";
-        ctx.textAlign = "center";
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillText("WELCOME", canvas.width / 2, canvas.height / 1.22);
+        context.font = "70px HYWenHei";
+        context.textAlign = "center";
+        context.fillStyle = "#FFFFFF";
+        context.fillText("WELCOME", canvas.width / 2, canvas.height / 1.22);
 
         // Print Usertag Text
         do {
-            ctx.font = `${fontSize -= 10}px HYWenHei`;
-        } while (ctx.measureText(`${user.tag}`).width > canvas.width);
-        ctx.fillText(user.tag.toUpperCase(), canvas.width / 2, canvas.height / 1.1);
+            context.font = `${fontSize -= 10}px HYWenHei`;
+        } while (context.measureText(`${user.tag}`).width > canvas.width);
+        context.fillText(user.tag.toUpperCase(), canvas.width / 2, canvas.height / 1.1);
 
         // Set Profile Picture
-        const avatar = await Canvas.loadImage(user.displayAvatarURL({ format: "png" }));
-
         let x = canvas.width / 2 - 100
         let y = canvas.height / 2 - 100
-        ctx.beginPath();
-        ctx.arc(x + 100, y + 100, 100, 0, Math.PI * 2, true);
-        ctx.lineWidth = 15;
-        ctx.shadowBlur = 0;
-        ctx.strokeStyle = '#FFFFFF';
-        ctx.stroke();
-        ctx.closePath();
-        ctx.clip();
-        ctx.drawImage(avatar, x, y, 200, 200);
+        const avatar = await Canvas.loadImage(user.displayAvatarURL({ format: "png" }));
 
-        const attachment = new AttachmentBuilder(canvas.toBuffer(), { name: 'welcome.png' });
+        context.beginPath();
+        context.arc(x + 100, y + 100, 100, 0, Math.PI * 2, true);
+        context.lineWidth = 15;
+        context.shadowBlur = 0;
+        context.strokeStyle = '#FFFFFF';
+        context.stroke();
+        context.closePath();
+        context.clip();
+        context.drawImage(avatar, x, y, 200, 200);
 
-        // TEST SENDING CONSOLE.LOG 3
-        console.log("EXECUTE 3")
+        const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'welcome.png' });
 
         // Set Welcome Embed
         const welcome = new EmbedBuilder()
@@ -92,13 +81,10 @@ module.exports = {
         .setTimestamp()
         .setFooter({ text: `Member #${guild.memberCount}`, iconURL: guild.iconURL() });
 
-        // TEST SENDING CONSOLE.LOG 4
-        console.log("EXECUTE 4")
-
         await webhook.send({
             content: `Welcome ${user} **(${user.tag})**,`, 
             embeds: [welcome], 
             files: [attachment]
-        }).catch((err) => console.log(err));
+        });
     }
 }
