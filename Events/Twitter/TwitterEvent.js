feaaec5190218ad278304ce640be011d06efaee1
unknown <munajatabdulhafiz18@gmail.com>
2022-09-01 18:47:09 +0700

    fix twitter event

--- Events/Twitter/TwitterEvent.js
+++ Events/Twitter/TwitterEvent.js
@@ -1,26 +1,27 @@
 const { twitterPost } = require("../../Functions/Twitter/TwitterPost")
 const Twitter = require("twitter")
-require("dotenv").config()
+require("dotenv").config();
 
-module.exports = {
-    name: "twitterEvent",
-    async execute() {
-        const twitter = new Twitter({
-            consumer_key: process.env.TWITTER_CONSUMER_KEY,
-            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-            access_token_key: process.env.TWITTER_TOKEN_KEY,
-            access_token_secret: process.env.TWITTER_TOKEN_SECRET,
-        })
+module.exports.name = "twitterEvent"
 
-        twitter.stream('statuses/filter', { follow: '306490355' }, function(stream) {
-            stream.on("data", function(tweet) {
-                if (!tweet.retweeted_status & !tweet.in_reply_to_user_id)
-                    return twitterPost(tweet)
-            })
+const twitter = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_TOKEN_SECRET,
+})
 
-            stream.on('error', function(error) {
-                console.log(`An error occured on stream.on: 'error' \n${error}`)
-            })
-        })
-    }
-}
+twitter.stream('statuses/filter', { follow: '306490355' }, function(stream) {
+    stream.on("data", function(tweet) {
+        try {
+            if (!tweet.retweeted_status & !tweet.in_reply_to_user_id)
+                return twitterPost(tweet)
+        } catch (error) {
+            console.log(`An error occured on stream.on: 'data'\n${error}`)
+        }
+    })
+
+    stream.on('error', function(error) {
+        console.log(`An error occured on stream.on: 'error' \n${error}`)
+    });
+})
