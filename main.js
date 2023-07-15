70c7cd91cec2b40baf0bc8c23bb4901408232a97
unknown <munajatabdulhafiz18@gmail.com>
2023-07-15 17:48:30 +0700

    remove connect database and and music detection

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ client.player.events.on('disconnect', (queue, track) => {
 client.player.events.on('error', (queue, error) => console.log(`[Discord] Error emitted from the queue: ${error.message}`))
 
 // log in the client using the bot token
-client.login(process.env.BOT_TOKEN_SSS).then(() => {
+client.login(process.env.BOT_TOKEN).then(() => {
     loadEvents(client)
     loadCommands(client)
     loadComponents(client)
